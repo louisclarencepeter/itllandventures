e3aa27307e83baf559ee2d1b067450a866527016
Louis Peter <109584572+louisclarencepeter@users.noreply.github.com>
2024-05-24 10:53:47 +0200

    added the confirgurations

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,24 +1,46 @@
+const CACHE_NAME = 'my-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/logo192.png',
+  '/logo512.png',
+  // Add more assets to cache during installation
+];
+
 self.addEventListener('install', function(event) {
-    // Perform install steps
-    event.waitUntil(
-      caches.open('my-cache').then(function(cache) {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/manifest.json',
-          '/logo192.png',
-          '/logo512.png',
-          // Add more assets to cache during installation
-        ]);
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
       })
-    );
-  });
-  
-  self.addEventListener('fetch', function(event) {
-    // Intercept fetch requests and serve cached assets if available
-    event.respondWith(
-      caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
       })
-    );
-  });
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  var cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
