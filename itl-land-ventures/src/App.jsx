5a4e0ac7b9b964f6f972babf786edb3e0aa14b47
Louis Peter <109584572+louisclarencepeter@users.noreply.github.com>
2024-05-24 05:32:08 +0200

    intial commit

---
+++ itl-land-ventures/src/App.jsx
@@ -0,0 +1,35 @@
+import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram, faFacebook, faWhatsapp, faYoutube, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import logo from '../public/logo.png';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>ITL Land Ventures</h1>
+        <p>Our website is currently under development. Stay connected with us through our social media channels!</p>
+        <div className="social-links">
+          <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faInstagram} size="2x" />
+          </a>
+          <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faFacebook} size="2x" />
+          </a>
+          <a href="https://wa.me" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faWhatsapp} size="2x" />
+          </a>
+          <a href="https://youtube.com" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faYoutube} size="2x" />
+          </a>
+          <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faTwitter} size="2x" />
+          </a>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
