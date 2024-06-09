88791d71ec55079fcd1741b2413dfc83db67bd09
Louis Peter <109584572+louisclarencepeter@users.noreply.github.com>
2024-06-09 17:41:35 +0200

    removed the uneeded styles

--- src/App.jsx
+++ src/App.jsx
@@ -19,48 +19,6 @@ function App() {
   }, []);
 
   useEffect(() => {
-    const initializeCMP = async () => {
-      if (window.UC_UI) {
-        await window.UC_UI.initialize();
-      } else {
-        const script = document.getElementById('usercentrics-cmp');
-        if (script) {
-          script.onload = async () => {
-            if (window.UC_UI) {
-              await window.UC_UI.initialize();
-            }
-          };
-        }
-      }
-    };
-
-    const initializeChatbase = () => {
-      const existingScript = document.querySelector('script[src="https://www.chatbase.co/embed.min.js"]');
-      if (!existingScript) {
-        const script = document.createElement('script');
-        script.src = "https://www.chatbase.co/embed.min.js";
-        script.setAttribute('chatbotId', '_uKBoYS6yXAh9HztcRRrK');
-        script.setAttribute('domain', 'www.chatbase.co');
-        script.setAttribute('data-namespace', 'chatbase');
-        script.async = true;
-        document.body.appendChild(script);
-        window.embeddedChatbotConfig = {
-          chatbotId: "_uKBoYS6yXAh9HztcRRrK",
-          domain: "www.chatbase.co",
-          config: {
-            iconWidth: 35,
-            iconHeight: 35,
-            iconBackground: '#282c34',
-          }
-        };
-      }
-    };
-
-    initializeCMP();
-    initializeChatbase();
-  }, []);
-
-  useEffect(() => {
     const googleTagManagerScript = document.createElement("script");
     googleTagManagerScript.async = true;
     googleTagManagerScript.src = "https://www.googletagmanager.com/gtag/js?id=YOUR_GOOGLE_TAG_MANAGER_ID";
