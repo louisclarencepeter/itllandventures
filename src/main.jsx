8c24dcd15ea1c7d5a4404697e49be6e74b300ff3
Louis Peter <louisclarencepeters@gmail.com>
2024-06-14 20:54:58 +0200

    added new links

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-// Register service worker if supported
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/service-worker.js')
@@ -16,7 +15,7 @@ if ('serviceWorker' in navigator) {
   });
 }
 
-// Render the React application
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
